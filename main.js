a7f9ecc1bea5a101ad5e86062af7af37ac1d0dee
YeojinMAC <yeojiinchoii@gmail.com>
2022-12-07 11:11:40 -0500

    fixing

---
+++ main.js
@@ -0,0 +1,39 @@
+// $(function() {
+  
+//     var winHeight = $(window).height(),
+//       eleOffsetTop = $(".counter").offset().top,
+//       eleTop = eleOffsetTop - winHeight,
+//       current = 0;
+  
+//     $(window).on("scroll", function() {
+//       var scrollTop = $(window).scrollTop();
+  
+//       if (current == 0 && scrollTop >= eleTop) {
+        
+  
+//           $('.counter div').each(function() {
+//     var $this = $(this),
+//         countTo = $this.attr('data-count');
+    
+//     $({ countNum: $this.text()}).animate({
+//       countNum: countTo
+//     },
+  
+//     {
+  
+//       duration: 2000,
+//       easing:'linear',
+//       step: function() {
+//         $this.text(Math.floor(this.countNum));
+//       },
+//       complete: function() {
+//         $this.text(this.countNum);
+//       }
+  
+//     });  
+//   });  
+//       }
+   
+//     });
+//   });
+  
